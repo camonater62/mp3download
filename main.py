ec977af3c68b72c06f4b0887fe9c9926b2ea4573
Cameron West <camonater62@gmail.com>
2023-03-02 19:49:48 -0800

    ytdl endpoint

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 from __future__ import unicode_literals
 from http.server import BaseHTTPRequestHandler, HTTPServer, SimpleHTTPRequestHandler
-from urllib.parse import urlparse
-from urllib.parse import parse_qs
+from urllib.parse import urlparse, unquote
 import subprocess
+from pathlib import PurePosixPath
 
 hostName = "localhost"
 serverPort = 8080
 
 class MyServer(SimpleHTTPRequestHandler):
     def handle_download(self, url):
-        if len(url) == 0:
-            return
-        
-        source = subprocess.check_output(["yt-dlp", "--get-url", "-f", "bestaudio/best", url])
-        source = source.decode()
+        try:
+            url = unquote(url)
 
-        self.send_response(301)
-        self.send_header("Location", source)
-        self.end_headers()
-        
+            source = subprocess.check_output(["yt-dlp", "--no-playlist", "--get-url", "-f", "bestaudio/best", url])
+            source = source.decode()
+
+            self.send_response(301)
+            self.send_header("Location", source)
+            self.end_headers()
+        except:
+            self.send_error(400)
 
     def do_GET(self):
-        query_components = parse_qs(urlparse(self.path).query)
-        if "url" in query_components:
-            self.handle_download(query_components["url"][0])
-        else:
-            pass
+        parts = PurePosixPath(
+            unquote(
+                urlparse(
+                    self.path
+                ).path
+            )
+        ).parts
+
+        if (parts[1] == "ytdl"):
+            self.handle_download(self.path[6:])
 
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
